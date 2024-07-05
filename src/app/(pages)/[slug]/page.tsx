022b1e312b4a9397699eb680d004cd337ce67be9
MarvinRF <mamavivi@gmail.com>
2024-07-04 23:40:38 -0300

    categories & section done

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -19,7 +19,9 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // See https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
-import classes from '.index.module.scss'
+import Categories from '../../_components/Categories'
+
+import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -61,8 +63,8 @@ export default async function Page({ params: { slug = 'home' } }) {
         <section>
           <Hero {...hero} />
 
-          <Gutter>
-            <p>Helo world</p>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
           </Gutter>
         </section>
       ) : (
